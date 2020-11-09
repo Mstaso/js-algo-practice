1ab1ae63b0d4c503b849b33e5d456d7441b6702e
malcolm <mstaso1@gmail.com>
2020-11-09 12:53:31 -0500

    fizzbuzz

--- practicejs.js
+++ practicejs.js
@@ -58,4 +58,20 @@ function reverseInt(n) {
         }
     
         return maxChar;
+    }  
+
+    function fizzBuzz(n) {
+
+        for (let i=1; i <= n; i++){
+            // check to see if number is a multiple of both 3 & 5?
+            if (i % 3 === 0 && i % 5 === 0){
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0){
+                console.log("fizz");
+            } else if (i % 5 === 0){
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
     }    
