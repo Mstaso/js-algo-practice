5f0e4c9bfc2b441a04afd68e94785f7f95281e61
malcolm <mstaso1@gmail.com>
2020-11-09 12:53:01 -0500

    max char

--- practicejs.js
+++ practicejs.js
@@ -36,3 +36,26 @@ function reverseInt(n) {
     // }
     return reversedNumber * Math.sign(n);
     }
+
+    function maxChar(str) {
+        const charMap = {};
+        let max = 0;
+        let maxChar = '';
+    
+        for(let char of str){
+            if(charMap[char]){
+                charMap[char]++;
+            } else {
+                charMap[char]=1;
+            }
+        } 
+       
+        for(let char in charMap){
+            if (charMap[char] > max){
+                max = charMap[char]
+                maxChar = char;
+            }
+        }
+    
+        return maxChar;
+    }    
