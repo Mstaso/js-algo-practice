25641a314437a7f4ad172f7cc7d2577bf5ef7ea8
malcolm <mstaso1@gmail.com>
2020-11-10 13:42:54 -0500

    capitalize p2

--- practicejs.js
+++ practicejs.js
@@ -149,4 +149,19 @@ function reverseInt(n) {
             }
             return words.join(' ')
         }
+
+    // capitalize p2
+    function capitalize(str) {
+        let result = str[0].toUpperCase();
+      
+        for(let i = 1; i < str.length; i++) {
+            if (str[i - 1] === ' ') {
+                result += str[i].toUpperCase();
+            } else {
+                result += str[i]
+            }
+        }
+      
+        return result
+          } 
        
