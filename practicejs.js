9174ca54e75b7a8beef791b841e359879aea8d01
malcolm <mstaso1@gmail.com>
2020-11-12 16:53:19 -0500

    pyramid part 2

--- practicejs.js
+++ practicejs.js
@@ -203,3 +203,22 @@ function reverseInt(n) {
             }
         }
         
+        // Pyramid Part 2
+
+        function pyramid(n, row=0, level = '') {
+            if (row === n) {
+                return;
+            }
+            if (level.length === 2 * n -1){
+                console.log(level);
+                return pyramid(n, row +1);
+            }
+
+            const midpoint = Math.floor((2 * n - 1) / 2);
+            let add;
+            if (midpoint - row <= level.lengt && midpoint + row >= level.length)
+            {add = '#';}
+            else {
+                add = '';
+            }
+        } 
