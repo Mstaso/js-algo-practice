61423f45eeb1ff812cf5a52c075e809bef916434
malcolm <mstaso1@gmail.com>
2020-11-18 16:34:35 -0500

    remove first, clear, getlast, getfirst

--- practicejs.js
+++ practicejs.js
@@ -572,3 +572,55 @@ class LinkedList {
     }
 
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+
+    }
+
+    size() {
+        let counter = 0
+        let node = this.head
+        while(node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+
+        if(!this.head) {
+            return null;
+        }
+        let node = this.head
+        while(node) {
+            if(!node.next){
+               return node;
+            } 
+            node = node.next
+            
+        }
+    }
+
+    clear() {
+        this.head = null
+    }
+
+    removeFirst() {
+        this.head = this.head.next
+    }
+
+
+
+}
