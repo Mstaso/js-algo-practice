d983db4a2af2c720864b5ec208078e0239fb4710
malcolm <mstaso1@gmail.com>
2020-11-19 12:33:22 -0500

    midpoint

--- practicejs.js
+++ practicejs.js
@@ -704,5 +704,17 @@ class LinkedList {
         }
     }
 
+    
 
+
+}
+function midpoint(list) {
+    let slow = list.head;
+    let fast = list.head;
+
+    while(fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
 }
