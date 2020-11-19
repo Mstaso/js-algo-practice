10b4b6af5007414fc2b87610159866bf4fd9d6a2
malcolm <mstaso1@gmail.com>
2020-11-19 12:44:54 -0500

    circular

--- practicejs.js
+++ practicejs.js
@@ -718,3 +718,18 @@ function midpoint(list) {
     }
     return slow;
 }
+
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+
+    while(fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            return true;
+        }
+    }
+    return false;
+
+}
