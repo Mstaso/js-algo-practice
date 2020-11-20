6455d3c65bb68bb3e46034bd732ee3a1872668c6
malcolm <mstaso1@gmail.com>
2020-11-20 15:39:30 -0500

    node p1

--- practicejs.js
+++ practicejs.js
@@ -733,3 +733,21 @@ function circular(list) {
     return false;
 
 }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];    
+    }
+
+    add(data) {
+        this.children.push(new Node(data));
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        })
+    }
+
+}
