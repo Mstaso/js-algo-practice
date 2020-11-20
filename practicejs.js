24cf2bbe968838e518df11d883593073d3bd3446
malcolm <mstaso1@gmail.com>
2020-11-20 16:00:41 -0500

    traverseBF

--- practicejs.js
+++ practicejs.js
@@ -750,4 +750,13 @@ class Node {
         })
     }
 
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+             arr.push(...node.children);
+             fn(node);
+        }
+     }
+
 }
