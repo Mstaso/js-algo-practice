46abd4a64bef787ec166984c9f94a4b11a9c61e3
malcolm <mstaso1@gmail.com>
2021-01-18 15:47:05 -0500

    including notes

--- newpractice.js
+++ newpractice.js
@@ -2,5 +2,25 @@
 // Approach to solving problems
 
 // What is an Algorithm
-// A process or set of steps to solve a problem
+// A process or set of steps to accomplish a certain task
 // 
+
+// Why Do I need to know this?
+// Almost everything that you do in programming involves some kind of algorithn!
+// its the foundation for being a successful problem solving developer
+// interviews
+
+// How do you improve
+
+// Devise a plan for solving problems
+// Master common problem solving patterns
+
+
+// Problem Solving Approach
+
+// Understand the Problem
+
+// Explore Concrete Examples
+// Break it Down
+// Solve/Simplify
+// Look Back and Refactor 
