d616d187187096d4fe5d6f8b622336c330018445
malcolm <mstaso1@gmail.com>
2021-01-19 14:45:00 -0500

    are there duplicates

--- newpractice.js
+++ newpractice.js
@@ -198,6 +198,17 @@ function sameFrequency(int1, int2) {
     return true;
 }
 
+function areThereDuplicates() {
+    let collection = {}
+    for(let val in arguments){
+      collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for(let key in collection){
+      if(collection[key] > 1) return true
+    }
+    return false;
+  }
+
 
 
 
