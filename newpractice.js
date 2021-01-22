f56e3f0348e94233f051031fc776e6ba53a57376
malcolm <mstaso1@gmail.com>
2021-01-22 16:18:23 -0500

    subsequencep2

--- newpractice.js
+++ newpractice.js
@@ -462,3 +462,15 @@ function averagePair(arr, num){
     return false;
   }
 
+  function isSubsequence(str1, str2) {
+    // create two pointers
+    let i = 0;
+    let j = 0;
+    while(j < str2.length){
+        if (str2[j] === str1[i]) i++;
+        if (i === str1.length) return true;
+        j++;
+    }
+    
+    return false;
+}
