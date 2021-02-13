8d39972d01c0abc30751fe67044958596ab51e72
malcolm <mstaso1@gmail.com>
2021-02-13 14:50:56 -0500

    python practice

---
+++ views.py
@@ -0,0 +1,22 @@
+a = 20
+
+b = 2
+
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a > b + 10:
+    print("a is greater than b plus 10")    
+else:
+    print("a is greater than b")    
+
+name = 'yk'
+height_m = 2
+weight_kg = 90
+
+bmi = weight_kg / (height_m ** 2)
+print (bmi)
+
+# comment check 
+
