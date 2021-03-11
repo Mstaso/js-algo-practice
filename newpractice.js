dc81bb3a9d561b494138b66ef74d56373a19650c
malcolm <mstaso1@gmail.com>
2021-03-11 14:45:29 -0500

    factorial

--- newpractice.js
+++ newpractice.js
@@ -480,3 +480,11 @@ function sumRange(num){
     if(num ===1) return 1;
     return num + sumRange(num-1);
 }
+
+function factorial(num){
+    let total = 1;
+    for(let i = num; i > 0; i--){
+        total *= i
+    }
+    return total
+}
