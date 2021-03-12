07784e9c1d384e5c13b6a67776019cd1a504066c
malcolm <mstaso1@gmail.com>
2021-03-12 12:14:13 -0500

    productOfArray

--- newpractice.js
+++ newpractice.js
@@ -493,3 +493,18 @@ function power(base, exponent) {
     if(exponent === 0) return 1;
     return base * power(base, exponent-1)
 }
+
+function factorial(num){
+    let total = 1;
+    for (let i =num; i > 1; i--){
+        total *= i
+    }
+    return total
+}
+
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
