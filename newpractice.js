515c3e3a51a7905369b6741b62892bb6ab576fff
malcolm <mstaso1@gmail.com>
2021-03-19 15:40:55 -0400

    some-recursive

--- newpractice.js
+++ newpractice.js
@@ -532,3 +532,9 @@ function isPalindrome(str){
 }
 
 isPalindrome('check');
+
+function someRecursive(array, callback) {
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursive(array.slice(1),callback);
+}
