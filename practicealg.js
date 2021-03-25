9b5eece463a3159c8d5d8761a4284f053515a9ff
malcolm <mstaso1@gmail.com>
2021-03-24 18:04:33 -0600

    reverasl and palindrome

--- practicealg.js
+++ practicealg.js
@@ -1,6 +1,34 @@
-function reverse(str) {
-   return str.split('').reverse().join() 
+// function reverse(str) {
+//    return str.split('').reverse().join('')
    
+// }
+
+// function reverse(str) {
+//   let reversed = ''
+//   for(let char of str){
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+//  }
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, character) => {
+//         return character + reversed;
+//     }, '')
+    
+//  }
+
+ function palindrome(str) {
+    // create an empty string variable 
+    let reversed = '';
+      // iterate through str and add character to the variable created earlier
+    for(let char of str){
+        reversed = char + reversed;
+    }
+    //  compare
+   return reversed === str ? true : false;
 }
 
 reverse("apple");
+
