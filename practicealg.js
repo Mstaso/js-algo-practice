4600c36c58c4b237e89b8d8e8f56c3963581e753
malcolm <mstaso1@gmail.com>
2021-04-03 10:50:51 -0400

    parkingsystem

--- practicealg.js
+++ practicealg.js
@@ -458,3 +458,24 @@ var kidsWithCandies = function(candies, extraCandies) {
         
         return result;
     };
+
+
+    class ParkingSystem {
+        constructor(big, medium, small) {
+           this.slots = {
+               1: big,
+               2: medium,
+               3: small
+           }
+        } 
+        
+            addCar(carType) {
+               if(this.slots[carType] === 0) {
+                   return false;
+               }
+               
+               this.slots[carType]--;
+               return true;
+           };
+           
+       }
