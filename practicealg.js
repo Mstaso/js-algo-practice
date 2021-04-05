4e5f0870f2998237b0de3640bbd042daf65cec80
malcolm <mstaso1@gmail.com>
2021-04-05 19:50:19 -0400

    countgoodtriplets

--- practicealg.js
+++ practicealg.js
@@ -614,3 +614,25 @@ var kidsWithCandies = function(candies, extraCandies) {
                                 
                                 return matches;
                             };                        
+
+
+                            var countGoodTriplets = function(arr, a, b, c) {
+                                //     create counter variable
+                                    let counter = 0;
+                                //     loop through array and compare
+                                    
+                                //     if arr at indices meets condition, increment counter by 1
+                                    for(let i = 0; i < arr.length; i++) {
+                                        for(let j = i + 1; j < arr.length; j++) {
+                                            if(Math.abs(arr[i] - arr[j]) <= a) {
+                                                 for(let k = j + 1; k < arr.length; k++) {
+                                                    if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
+                                                        counter ++;
+                                                        }
+                                                    }        
+                                                }
+                                            }
+                                        }
+                                       return counter;
+                                    }
+                                
