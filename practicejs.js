572dea3c14454904ab692883921959c8565b665c
malcolm <mstaso1@gmail.com>
2021-04-05 17:39:39 -0400

    arraystringsequal

--- practicejs.js
+++ practicejs.js
@@ -971,4 +971,9 @@ var rangeSumBST = function(root, low, high) {
         };    
 
 
+        var arrayStringsAreEqual = function(word1, word2) {
 
+            return word1.join('') === word2.join('')
+            
+            };
+            
