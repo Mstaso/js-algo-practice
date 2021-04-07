54bc765a9ed2ff70bb94f2cc0b37b027941bfa07
malcolm <mstaso1@gmail.com>
2021-04-07 16:37:56 -0400

    countGoodRectangles

--- practicealg.js
+++ practicealg.js
@@ -791,5 +791,22 @@ var flipAndInvertImage = function(image) {
     }
 
     return image;
-};    
+}; 
+var countGoodRectangles = function(rectangles) {
+    let max = 0;
+    let count = 0;
+    
+    for(let i = 0; i < rectangles.length; i++) {
+        let minSide = Math.min(rectangles[i][0], rectangles[i][1])
+        
+        if(minSide > max) {
+            count = 0
+            max = minSide
+        } 
+        
+        if(minSide === max) count++
+    }
+    
+    return count
+};   
                                     
